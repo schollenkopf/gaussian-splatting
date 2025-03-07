8f464467296cd402e417f96982d851faa5fe417d
schollenkopf <nelson.becker@web.de>
2025-03-07 16:03:26 +0100

    added hpc script

---
+++ run_on_hpc.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# embedded options to bsub - start with #BSUB
+# -- our name ---
+#BSUB -J SingleCorePython 
+# -- choose queue --
+#BSUB -q hpc
+# -- specify that we need 4GB of memory per core/slot --
+# so when asking for 4 cores, we are really asking for 4*4GB=16GB of memory 
+# for this job. 
+#BSUB -R "rusage[mem=4GB]"
+# -- Notify me by email when execution begins --
+#BSUB -B
+# -- Notify me by email when execution ends   --
+#BSUB -N
+# -- email address -- 
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+#BSUB -u s194702@student.dtu.dk
+# -- Output File --
+#BSUB -o Output_%J.out
+# -- Error File --
+#BSUB -e Output_%J.err
+# -- estimated wall clock time (execution time): hh:mm -- 
+#BSUB -W 24:00 
+# -- Number of cores requested -- 
+#BSUB -n 1 
+# -- Specify the distribution of the cores: on a single node --
+#BSUB -R "span[hosts=1]"
+# -- end of LSF options -- 
+
+module load colmap gcc/10
+
+pip install submodules/diff-gaussian-rasterization
+pip install submodules/fused-ssim
+pip install submodules/simple-knn
+
+
+python train.py -s ../first3
+
