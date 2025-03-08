d04cf05126f52138cc35e10666f682a3c22f1164
schollenkopf <nelson.becker@web.de>
2025-03-08 18:52:27 +0100

    4cores

--- run_on_hpc.sh
+++ run_on_hpc.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 # embedded options to bsub - start with #BSUB
 # -- our name ---
-#BSUB -J SingleCorePython 
-# -- choose queue --
-#BSUB -q hpc
-# -- specify that we need 4GB of memory per core/slot --
-# so when asking for 4 cores, we are really asking for 4*4GB=16GB of memory 
-# for this job. 
+#BSUB -J gpuCorePython 
+#BSUB -q GPUqueuename
+### request the number of GPUs
+#BSUB -gpu "num=1:mode=exclusive_process"
+### request the number of CPU cores (at least 4x the number of GPUs)
+#BSUB -n 4 
+### we want to have this on a single node
+#BSUB -R "span[hosts=1]"
+### we need to request CPU memory, too (note: this is per CPU core)
 #BSUB -R "rusage[mem=4GB]"
-# -- Notify me by email when execution begins --
 #BSUB -B
 # -- Notify me by email when execution ends   --
 #BSUB -N
-# -- email address -- 
-# please uncomment the following line and put in your e-mail address,
-# if you want to receive e-mail notifications on a non-default address
 #BSUB -u s194702@student.dtu.dk
 # -- Output File --
 #BSUB -o Output_%J.out
@@ -22,12 +21,7 @@
 #BSUB -e Output_%J.err
 # -- estimated wall clock time (execution time): hh:mm -- 
 #BSUB -W 24:00 
-# -- Number of cores requested -- 
-#BSUB -n 4 
-# -- Specify the distribution of the cores: on a single node --
-#BSUB -R "span[hosts=1]"
-# -- end of LSF options -- 
-#BSUB -gpu "num=1:mode=exclusive_process"
+
 
 module load colmap gcc/10
 
